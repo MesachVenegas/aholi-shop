25b5531a1c19cf3ddd835aac7213fe44eb4c6161
MesachVenegas <mesach.venegas@hotmail.com>
2023-12-14 12:12:08 -0800

    create font exporter and fonts

---
+++ components/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Cedarville_Cursive, Recursive } from 'next/font/google';
+
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const recursive = Recursive({ subsets: ['latin']});
+
+export const cedarville_cursive = Cedarville_Cursive({
+  weight: '400',
+  subsets: ['latin']
+});
