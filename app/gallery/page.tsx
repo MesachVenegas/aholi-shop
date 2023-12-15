b47ea1ce5f61bee1171cfc7229d8b55010ba7a23
MesachVenegas <mesach.venegas@hotmail.com>
2023-12-15 00:17:40 -0800

    add gallery page content

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import GalleryViewer from "@/components/Gallery/GalleryViewer";
+import { CursiveTitle } from "@/components/ui";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: 'Aholi | Gallery',
+  description: 'Galeria de proyectos realizados por aholi, donde se muestran la variedad de decoraciones, realizadas para diferente eventos',
+  keywords: ['arreglos', 'decoracion', 'centros de mesa', 'xv años', 'decoraciones', 'bodas', 'graduaciones', 'gallery', 'events', 'flowers'],
+}
 export default function GalleryPage() {
   return (
-    <div>GalleryPage</div>
+    <main className="flex flex-col justify-center items-center gap-8 p-6">
+      <div className="flex flex-col justify-center items-center w-full">
+        <CursiveTitle size="text-6xl z-40" >Galeria de Proyectos</CursiveTitle>
+        <hr className="w-full max-w-3xl text-rose-700"/>
+      </div>
+      <div className="w-full h-full p-4">
+        <GalleryViewer />
+      </div>
+    </main>
   )
 }
