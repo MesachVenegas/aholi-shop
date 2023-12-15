f2d170fa9329b8980a75cb7985a04d4911c387c9
MesachVenegas <mesach.venegas@hotmail.com>
2023-12-14 20:08:06 -0800

    add social link, icon definitions and colors

--- libs/constants.ts
+++ libs/constants.ts
@@ -1,5 +1,15 @@
+import { faFacebook, faTiktok, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope, faHome, faImages, faShop } from "@fortawesome/free-solid-svg-icons";
+
 export const menuLinks = [
-  { name: 'Inicio', url: '/' },
-  { name: 'Sobre Aholi', url: '/about' },
-  { name: 'Tienda', url: '/shop' },
+  { name: 'Inicio', url: '/', icon: faHome },
+  { name: 'Galeria', url: '/gallery', icon: faImages },
+  { name: 'Tienda', url: '/shop', icon: faShop },
+  { name: 'Contacto', url: '/contact', icon: faEnvelope },
+]
+
+export const socialLinks = [
+  { name: 'Whatsapp', url: '#', icon: faWhatsapp, color: 'text-emerald-400', hover: 'text-emerald-300' },
+  { name: 'Tiktok', url: '#', icon: faTiktok, color: 'text-slate-800', hover: 'text-slate-700' },
+  { name: 'Facebook', url: '#', icon: faFacebook, color: 'text-blue-500', hover: 'text-blue-400' },
 ]
