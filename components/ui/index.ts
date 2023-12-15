2b7f4f7a9831a14773aae73cd83cad8c595be922
MesachVenegas <mesach.venegas@hotmail.com>
2023-12-14 20:09:23 -0800

    create ui indexerd

---
+++ components/ui/index.ts
@@ -0,0 +1,15 @@
+import { CursiveTitle } from './CursiveTitle';
+import { Input } from '@/components/ui/Input';
+import { ButtonLink } from '@/components/ui/ButtonLink';
+import { Title2, Title3, Title4, Title5 } from '@/components/ui/Titles'
+
+
+export {
+  Title2,
+  Title3,
+  Title4,
+  Title5,
+  CursiveTitle,
+  ButtonLink,
+  Input,
+}
