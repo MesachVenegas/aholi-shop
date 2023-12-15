3327ff40bee5426cd40bf8c1cc8ca6bdbc353b66
MesachVenegas <mesach.venegas@hotmail.com>
2023-12-14 20:08:33 -0800

    add active menu link

--- components/nabvar/Navbar.tsx
+++ components/nabvar/Navbar.tsx
@@ -1,10 +1,16 @@
+'use client'
+
 import { menuLinks } from "@/libs/constants";
 import { faBagShopping, faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { usePathname } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Navbar() {
+  const pathname = usePathname();
+
+
   return (
     <nav className="flex justify-center text-white items-center w-full h-16 p-5 bg-rose-100 font-semibold">
       <div className="flex-grow">
@@ -23,7 +29,12 @@ export default function Navbar() {
         <ul className="flex-grow flex gap-4">
           {
             menuLinks.map(link =>(
-              <li key={link.name} className="relative text-md w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-white after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left">
+              <li
+                key={link.name}
+                className={
+                  `relative text-md w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-white after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left ${pathname === link.url ? 'after:block after:absolute after:h-[3px] after:w-full after:bg-white after:scale-x-100' : ''}`
+                }
+              >
                 <Link href={link.url}>
                   {link.name}
                 </Link>
