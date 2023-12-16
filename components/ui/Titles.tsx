c6f0c116cb2648764cae951e5b2238c3ce480b2d
MesachVenegas <mesach.venegas@hotmail.com>
2023-12-15 22:40:51 -0800

    change children type property

--- components/ui/Titles.tsx
+++ components/ui/Titles.tsx
@@ -1,32 +1,32 @@
 import { recursive } from './fonts'
 
-export function Title2({ children, color } : { children: string, color?: string} ) {
+export function Title2({ children, color } : { children: React.ReactNode, color?: string} ) {
   return (
-    <h2 className={`${recursive.className} text-2xl md:text-4xl uppercase tracking-widest font-semibold ${color ? color : 'text-slate-800'}`}>
+    <h2 className={`${recursive.className} flex gap-2 text-2xl md:text-4xl uppercase tracking-widest font-semibold ${color ? color : 'text-slate-800'}`}>
       {children}
     </h2>
   )
 }
 
-export function Title3({ children, color } : { children: string, color?: string} ) {
+export function Title3({ children, color } : { children: React.ReactNode, color?: string} ) {
   return (
-    <h3 className={`${recursive.className} text-lg md:text-2xl uppercase tracking-widest font-semibold ${color ? color : 'text-slate-800'}`}>
+    <h3 className={`${recursive.className} flex gap-2 text-lg md:text-2xl uppercase tracking-widest font-semibold ${color ? color : 'text-slate-800'}`}>
       {children}
     </h3>
   )
 }
 
-export function Title4({ children, color } : { children: string, color?: string} ) {
+export function Title4({ children, color } : { children: React.ReactNode, color?: string} ) {
   return (
-    <h3 className={`${recursive.className} text-md md:text-lg uppercase tracking-widest font-semibold ${color ? color : 'text-slate-800'}`}>
+    <h3 className={`${recursive.className} flex gap-2 text-md md:text-lg uppercase tracking-widest font-semibold ${color ? color : 'text-slate-800'}`}>
       {children}
     </h3>
   )
 }
 
-export function Title5({ children, color } : { children: string, color?: string} ) {
+export function Title5({ children, color } : { children: React.ReactNode, color?: string} ) {
   return (
-    <h3 className={`${recursive.className} text-sm md:text-base uppercase tracking-widest font-semibold ${color ? color : 'text-slate-800'}`}>
+    <h3 className={`${recursive.className} flex gap-2 text-sm md:text-base uppercase tracking-widest font-semibold ${color ? color : 'text-slate-800'}`}>
       {children}
     </h3>
   )
