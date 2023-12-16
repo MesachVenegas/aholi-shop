436d17330a4aa5cf5b6df2b7cd584a600a59dcae
MesachVenegas <mesach.venegas@hotmail.com>
2023-12-15 22:41:19 -0800

    add style to input component

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -3,7 +3,7 @@ interface Props extends React.InputHTMLAttributes<HTMLInputElement> {}
 export function Input(props: Props) {
   return (
     <input
-      className=""
+      className="bg-rose-700/50 px-3 py-1 rounded-lg text-white w-full max-w-sm"
       {...props}
     />
   )
