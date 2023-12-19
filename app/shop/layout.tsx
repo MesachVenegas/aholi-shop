6eb10d984bc97fa50259b7730faf5f9be34910aa
MesachVenegas <mesach.venegas@hotmail.com>
2023-12-18 20:36:06 -0800

    create  shop layout

---
+++ app/shop/layout.tsx
@@ -0,0 +1,12 @@
+import AsideBar from "@/components/shop/asidebar/AsideBar";
+
+export default function layout({ children }: { children: React.ReactNode}) {
+  return (
+    <main className="flex flex-col lg:flex-row gap-2">
+      <AsideBar />
+      <section className="w-full">
+        { children }
+      </section>
+    </main>
+  )
+}
