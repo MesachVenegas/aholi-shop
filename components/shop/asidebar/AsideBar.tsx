406658927c8bb5c1a4c445f253d49138e8fd719b
MesachVenegas <mesach.venegas@hotmail.com>
2023-12-18 20:34:48 -0800

    create aside bar component

---
+++ components/shop/asidebar/AsideBar.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import { useState } from "react";
+import { Title4 } from "@/components/ui";
+import AsideButton from "@/components/ui/AsideButton";
+import { categories, formas } from "@/libs/constants";
+
+
+export default function AsideBar() {
+
+
+  return (
+    <aside className="flex flex-col gap-6 w-full lg:w-72 lg:h-screen p-4">
+      <div className="flex flex-col gap-3">
+        <div>
+          <Title4 color="text-rose-700" >Categorías</Title4>
+          <hr className="w-full text-rose-100" />
+        </div>
+        <ul className="flex flex-wrap gap-2">
+          {
+            categories.map((category, index) => (
+              <li key={index} >
+                <AsideButton>
+                  { category.name }
+                </AsideButton>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+    </aside>
+  )
+}
