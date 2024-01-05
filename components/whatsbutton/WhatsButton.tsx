5d900815132064ec261523382a7eba8e6786db76
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-01-04 22:44:25 -0800

    changes styles by screen

--- components/whatsbutton/WhatsButton.tsx
+++ components/whatsbutton/WhatsButton.tsx
@@ -6,8 +6,11 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function WhatsButton() {
   return (
-      <Link href='https://api.whatsapp.com/send?phone=523321860284' className='flex justify-center items-center fixed bg-rose-700 border border-rose-100 w-20 h-20 rounded-full z-50 right-12 bottom-12 shadow-xl'>
-        <FontAwesomeIcon icon={faWhatsapp} className='w-12 h-12 text-white' />
+      <Link
+        href='https://api.whatsapp.com/send?phone=523321860284'
+        className='flex justify-center items-center fixed bg-rose-700 border border-rose-100 w-12 h-12 lg:w-20 lg:h-20 rounded-full z-50 right-4 bottom-1/2 shadow-xl'
+      >
+        <FontAwesomeIcon icon={faWhatsapp} className='w-8 h-8 lg:w-12 lg:h-12 text-white' />
       </Link>
   )
 }
