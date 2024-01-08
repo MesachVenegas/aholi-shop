5a8fe34588bd4d5dd5ce112493e61494f983fe51
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-01-08 06:11:28 -0800

    changes style

--- components/pagination/Pagination.tsx
+++ components/pagination/Pagination.tsx
@@ -24,7 +24,7 @@ export default function Pagination({ count } : { count: number}) {
   }
 
   return (
-    <div className="flex justify-between px-5 mt-5">
+    <div className="flex justify-between px-5 mt-7">
       <button
         className={`flex gap-2 items-center bg-rose-100 py-1 px-3 rounded-lg text-white hover:bg-rose-700 ${!hasPrev ? 'opacity-50 cursor-not-allowed' : null }`}
         disabled={!hasPrev}
