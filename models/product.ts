dc06e9fd82e6b67b4998d831aa6ab974c96e9e53
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-01-08 06:09:14 -0800

    add props models

--- models/product.ts
+++ models/product.ts
@@ -1,3 +1,5 @@
+import { IconDefinition } from "@fortawesome/free-brands-svg-icons";
+
 export interface ProductProps {
   title: string;
   images: {
@@ -10,3 +12,39 @@ export interface ProductProps {
   price: number;
   tags: string[];
 };
+
+export interface CategoriesProps {
+  name: string;
+  description?: string;
+  icon?: IconDefinition;
+}
+
+export interface SizesProps {
+  name: string;
+  width: string;
+  height: string;
+  type: string;
+}
+
+export interface ImagesProps {
+  index: number;
+  name: string;
+  url: string;
+  file: {
+    lastModified: number;
+    lastModifiedDate: Date;
+    name: string;
+    size: number;
+    type: string;
+    webkitRelativePath: string;
+  }
+}
+
+export type ProductFormPros = {
+  name: string;
+  price: string;
+  sizes: string;
+  category: string;
+  description: string;
+  images: ImagesProps[];
+}
