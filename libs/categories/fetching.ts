edb2ba1e334e72363705d2c4617d63d0905e930e
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-01-11 09:00:32 -0800

    changes import, user server and docs

--- libs/categories/fetching.ts
+++ libs/categories/fetching.ts
@@ -1,6 +1,15 @@
-import { prisma } from "../prisma";
+'use server'
 
+import prisma from "../prisma";
 
+/**
+ * Retrieves categories based on the provided search term and page number.
+ *
+ * @param search - The search term to filter categories by.
+ * @param page - The page number to retrieve categories from.
+ * @returns An array of categories that match the search term and page number.
+ * @throws Throws an error if there is an issue retrieving the categories.
+ */
 export async function getCategories( search: string, page: number) {
   const categoriesPerPage = 9;
 
@@ -23,11 +32,38 @@ export async function getCategories( search: string, page: number) {
 }
 
 
+/**
+ * Retrieves the count of categories from the database.
+ *
+ * @returns {Promise<number>} The count of categories.
+ * @throws {Error} If there is an error retrieving the count.
+ */
 export async function getCountCategories() {
   try {
     const count = await prisma.category.count();
 
     return count;
+  } catch (error) {
+    throw error;
+  }
+}
+
+
+/**
+ * Retrieves a list of categories from the database.
+ *
+ * @returns {Promise<Array<Category>>} A promise that resolves to an array of categories.
+ * @throws {Error} If there is an error retrieving the categories.
+ */
+export async function getCategorySelect() {
+  try {
+    const result = await prisma.category.findMany();
+
+    if (result) {
+      return result;
+    } else {
+      return [];
+    }
   } catch (error) {
     throw error;
   }
