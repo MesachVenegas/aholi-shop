7c56ea4bb3c383c71156e13fd9bb01c1270c1948
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-01-11 15:50:59 -0800

    change to use client component to server and fetching data

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,57 +1,31 @@
-'use client'
+import { ProductResponse } from "@/models/product";
+import SearchBar from "@/components/search/SearchBar";
+import { getProducts } from "@/libs/products/fetching";
+import Pagination from "@/components/pagination/Pagination";
+import CardProduct from "@/components/shop/cardProduct/CardProduct";
 
-import Image from "next/image";
-import { useState } from "react";
-import { Input, Title2, Title5 } from "@/components/ui";
-import { Label } from "@/components/ui/Label";
-import { products } from "@/libs/constants";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-export default function ShopPage() {
+export default async function ShopPage({ searchParams }: { searchParams: { product: string, page: number }}) {
+  const search = searchParams?.product || '';
+  const page = searchParams?.page || 1;
+  const products = await getProducts(search, page);
 
   return (
     <div className="flex flex-col justify-center items-center p-4  pb-12">
       <div className="flex w-full">
         <div className="flex-grow"></div>
         <div className="flex items-center w-full max-w-md">
-          <Label>
-            <FontAwesomeIcon icon={faSearch} className="2-5 h-5" />
-            <Input type="search" />
-          </Label>
+          <SearchBar placeholder="Buscar" query="product" />
         </div>
       </div>
-      <div className="flex flex-wrap justify-around gap-8 p-8">
+      <div className="flex flex-wrap justify-center xl:justify-start w-full gap-8 p-8">
         {
-          products.map( (product, index) => (
-            <figure key={index} className="flex flex-col p-2 w-96 h-[500px] border border-rose-700 rounded-lg cursor-pointer">
-              <div className="w-full h-[300px] relative">
-                <Image
-                  src={product.images[0].src}
-                  fill={true}
-                  alt={product.images[0].description}
-                />
-              </div>
-              <figcaption className="flex flex-col gap-2">
-                <Title5>{product.title}</Title5>
-                <p>{product.description}</p>
-                <span className="flex w-full justify-end text-2xl p-2">
-                  ${product.price.toFixed(2)} mxn
-                </span>
-                <div className="flex w-full gap-2">
-                  {
-                    product.tags.map( (tag, index) => (
-                      <span className="border border-rose-100 text-rose-700 cursor-pointer px-2 rounded-lg" key={tag}>{tag}</span>
-                    ))
-                  }
-                </div>
-                <button className="bg-rose-700/80 hover:bg-rose-700 p-2 rounded-md text-white w-full">
-                  Comprar
-                </button>
-              </figcaption>
-            </figure>
+          products.map( (product) => (
+            <CardProduct key={product.id} prod={product as unknown as ProductResponse} />
           ))
         }
+      </div>
+      <div className="flex items-center w-full max-w-7xl">
+        <Pagination count={0} />
       </div>
     </div>
   )
