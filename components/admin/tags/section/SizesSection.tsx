bcd0dc2e8059c3e35654d1bd9bd7e54060c8a54f
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-01-11 15:48:57 -0800

    change search params

--- components/admin/tags/section/SizesSection.tsx
+++ components/admin/tags/section/SizesSection.tsx
@@ -8,9 +8,9 @@ import AddSize from "../addTags/AddSize";
 export default async function SizesSection({
   searchParams
 }: {
-  searchParams: { search: string, page: number }
+  searchParams: { size: string, page: number }
 }) {
-  const search = searchParams?.search || '';
+  const search = searchParams?.size || '';
   const page = searchParams?.page || 1;
   const sizes = await getSizes(search, page);
   const count = await getCountSizes();
@@ -19,7 +19,7 @@ export default async function SizesSection({
   return (
     <section className="flex flex-col gap-2 p-4 shadow-2xl rounded-lg bg-white h-[87vh]">
       <div className="flex justify-between w-full p-2">
-        <SearchBar placeholder="Buscar Categoria" />
+        <SearchBar placeholder="Buscar Categoria" query="size" />
         <AddSize />
       </div>
       <div className="flex flex-col justify-between h-full w-full">
