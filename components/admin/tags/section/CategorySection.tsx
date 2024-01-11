a160e5a0e63a8bbdfd94e29953f5ecd926242002
MesachVenegas <mesach.venegas@hotmail.com>
2024-01-11 07:11:55 -0800

    add search function

--- components/admin/tags/section/CategorySection.tsx
+++ components/admin/tags/section/CategorySection.tsx
@@ -1,26 +1,31 @@
+import { ButtonLink } from "@/components/ui";
 import SearchBar from "@/components/search/SearchBar";
 import CategoriesTable from "../tables/CategoriesTable";
-import { categories } from "@/libs/constants";
-import Pagination from "@/components/pagination/Pagination";
-import { ButtonLink } from "@/components/ui";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAdd } from "@fortawesome/free-solid-svg-icons";
+import Pagination from "@/components/pagination/Pagination";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { getCategories, getCountCategories } from "@/libs/categories/fetching";
+import AddCategory from "../addTags/AddCategory";
 
-export default async function CategorySection() {
+export default async function CategorySection({ searchParams }: { searchParams: {search: string, page: number}}) {
+  const search = searchParams?.search || '';
+  const page = searchParams?.page || 1;
+  const categories = await getCategories(search, page);
+  const categoryCount = await getCountCategories();
 
   return (
     <section className="flex flex-col gap-2 p-4 shadow-2xl rounded-lg bg-white h-[87vh]">
       <div className="flex justify-between w-full p-2">
         <SearchBar placeholder="Buscar Categoria" />
-        <ButtonLink href="/admin/tags/addcategory" className="flex justify-center items-center gap-3 bg-rose-100/80 hover:bg-rose-100 px-3 rounded-lg text-white">
-          <FontAwesomeIcon className="w-4 h-5" icon={faAdd} />
-          Nueva Categoria
-        </ButtonLink>
+        <AddCategory />
       </div>
       <div className="flex flex-col justify-between h-full w-full">
         <CategoriesTable title="Categorías" tableData={ categories } />
-        <Pagination count={0} />
+        <Pagination count={categoryCount} />
       </div>
+      {
+
+      }
     </section>
   )
 }
