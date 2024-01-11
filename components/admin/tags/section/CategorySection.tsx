4b1c31d00c1aa55593205c7674ae5a81e7ea42dc
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-01-11 15:50:35 -0800

    changes query search

--- components/admin/tags/section/CategorySection.tsx
+++ components/admin/tags/section/CategorySection.tsx
@@ -1,14 +1,11 @@
-import { ButtonLink } from "@/components/ui";
 import SearchBar from "@/components/search/SearchBar";
 import CategoriesTable from "../tables/CategoriesTable";
-import { faAdd } from "@fortawesome/free-solid-svg-icons";
 import Pagination from "@/components/pagination/Pagination";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { getCategories, getCountCategories } from "@/libs/categories/fetching";
 import AddCategory from "../addTags/AddCategory";
 
-export default async function CategorySection({ searchParams }: { searchParams: {search: string, page: number}}) {
-  const search = searchParams?.search || '';
+export default async function CategorySection({ searchParams }: { searchParams: { category: string, page: number}}) {
+  const search = searchParams?.category || '';
   const page = searchParams?.page || 1;
   const categories = await getCategories(search, page);
   const categoryCount = await getCountCategories();
@@ -16,7 +13,7 @@ export default async function CategorySection({ searchParams }: { searchParams: 
   return (
     <section className="flex flex-col gap-2 p-4 shadow-2xl rounded-lg bg-white h-[87vh]">
       <div className="flex justify-between w-full p-2">
-        <SearchBar placeholder="Buscar Categoria" />
+        <SearchBar placeholder="Buscar Categoria" query="category" />
         <AddCategory />
       </div>
       <div className="flex flex-col justify-between h-full w-full">
