a55b8c71e0197e0f0f220e33a92f136f609b2434
MesachVenegas <mesach.venegas@hotmail.com>
2024-01-11 07:12:18 -0800

    add search function and modal to add size

--- components/admin/tags/section/SizesSection.tsx
+++ components/admin/tags/section/SizesSection.tsx
@@ -1,32 +1,30 @@
 import Pagination from "@/components/pagination/Pagination";
 import SearchBar from "@/components/search/SearchBar";
-import { ButtonLink } from "@/components/ui";
-import { faAdd } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import SizesTable from "../tables/SizesTables";
+import { getCountSizes, getSizes } from "@/libs/sizes/data";
+import AddSize from "../addTags/AddSize";
 
-const sizes = [
-  { name: 'Small', height: '28', width: '7', type: 'circular' },
-  { name: 'Small', height: '28', width: '7', type: 'circular' },
-  { name: 'Small', height: '28', width: '7', type: 'circular' },
-  { name: 'Small', height: '28', width: '7', type: 'circular' },
-  { name: 'Small', height: '28', width: '7', type: 'circular' },
-  { name: 'Small', height: '28', width: '7', type: 'circular' },
-  { name: 'Small', height: '28', width: '7', type: 'circular' },
-]
-export default async function SizesSection() {
+
+export default async function SizesSection({
+  searchParams
+}: {
+  searchParams: { search: string, page: number }
+}) {
+  const search = searchParams?.search || '';
+  const page = searchParams?.page || 1;
+  const sizes = await getSizes(search, page);
+  const count = await getCountSizes();
+
+
   return (
     <section className="flex flex-col gap-2 p-4 shadow-2xl rounded-lg bg-white h-[87vh]">
       <div className="flex justify-between w-full p-2">
         <SearchBar placeholder="Buscar Categoria" />
-        <ButtonLink href="/admin/tags/addcategory" className="flex justify-center items-center gap-3 bg-rose-100/80 hover:bg-rose-100 px-3 rounded-lg text-white">
-          <FontAwesomeIcon className="w-4 h-5" icon={faAdd} />
-          Nuevo tamaño
-        </ButtonLink>
+        <AddSize />
       </div>
       <div className="flex flex-col justify-between h-full w-full">
         <SizesTable title="Tamaños" tableData={ sizes } />
-        <Pagination count={0} />
+        <Pagination count={count} />
       </div>
     </section>
   )
