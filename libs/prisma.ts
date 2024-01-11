bb5f86a09adbdb8c7e112cbb4e9ad47d740bb2a8
MesachVenegas <mesach.venegas@hotmail.com>
2024-01-11 07:12:45 -0800

    prisma Init

--- libs/prisma.ts
+++ libs/prisma.ts
@@ -1,19 +1,28 @@
+/**
+ * Initializes a global instance of the PrismaClient from the @prisma/client package.
+ *
+ * @remarks
+ * This code snippet exports a `prisma` object that can be used to interact with the database using Prisma.
+ * It checks if a global `prisma` object already exists, and if not, creates a new instance of PrismaClient.
+ * In development mode, it assigns the `prisma` object to the global scope for easy access.
+ *
+ * @example
+ * //* Import the prisma object
+ * import { prisma } from './prisma';
+ *
+ * //* Use the prisma object to interact with the database
+ * const users = await prisma.user.findMany();
+ *
+ * @see {@link https://www.prisma.io/docs/concepts/components/prisma-client Prisma Client Documentation}
+ */
 import { PrismaClient } from "@prisma/client";
 
-type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
-
-const prismaClientSingleton = () => {
-  return new PrismaClient();
+declare global {
+  var prisma : PrismaClient | undefined;
 }
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClientSingleton | undefined;
-}
+export const prisma = global.prisma || new PrismaClient();
 
-const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
-
-export default prisma;
-
-if (process.env.NODE_ENV === 'production') {
-  globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') {
+  global.prisma = prisma;
 }
