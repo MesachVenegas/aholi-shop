99865fdfef16015cb3d0779f72f492867e1bdc0a
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-01-11 09:01:39 -0800

    add action to delete a item

--- components/admin/tags/tables/CategoriesTable.tsx
+++ components/admin/tags/tables/CategoriesTable.tsx
@@ -1,11 +1,22 @@
 'use client'
 
+import { delCategory } from "@/libs/categories/actions"
 import { CategoriesProps } from "@/models/product"
-import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons"
+import { faTrash } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { Card, Table, TableBody, TableCell, TableHead, TableHeaderCell, TableRow, Text, Title } from "@tremor/react"
 
 export default function CategoriesTable({ title, tableData }: { title: string, tableData: any[]}) {
+
+  const deleteCategory = async (id: number) => {
+    await delCategory(id)
+      .then( res => {
+        console.log(res);
+      }).catch( error => {
+        console.error(error);
+      } )
+  }
+
   return (
     <Card className="border border-rose-100/40 rounded-xl">
       <Title className="text-xl">{ title }</Title>
@@ -18,17 +29,16 @@ export default function CategoriesTable({ title, tableData }: { title: string, t
         </TableHead>
         <TableBody>
           {tableData.map((prod: CategoriesProps) => (
-            <TableRow key={prod.name} className="hover:bg-rose-100/10">
+            <TableRow key={prod.id} className="hover:bg-rose-100/10">
               <TableCell>{prod.name}</TableCell>
               <TableCell>
                 <Text>{prod.description}</Text>
               </TableCell>
               <TableCell className="flex gap-3">
-                  <span className="flex items-center gap-1 cursor-pointer text-rose-100">
-                    <FontAwesomeIcon icon={faEdit} className="w-4 h-4" />
-                    Editar
-                  </span>
-                  <span className="flex items-center gap-1 cursor-pointer text-red-600">
+                  <span
+                    className="flex items-center gap-1 cursor-pointer text-red-600"
+                    onClick={() => deleteCategory(prod.id)}
+                  >
                     <FontAwesomeIcon icon={faTrash} className="w-4 h-4" />
                     Eliminar
                   </span>
