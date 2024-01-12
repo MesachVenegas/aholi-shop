66ac5ef1f7a039cb2803f2bc3271a685dccd582f
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-01-11 16:04:13 -0800

    typo

--- app/shop/layout.tsx
+++ app/shop/layout.tsx
@@ -1,6 +1,7 @@
 import AsideBar from "@/components/shop/asidebar/AsideBar";
 
 export default function layout({ children }: { children: React.ReactNode}) {
+
   return (
     <main className="flex flex-col lg:flex-row gap-2">
       <AsideBar />
