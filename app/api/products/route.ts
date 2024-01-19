9bfd6faf6f73411e08f044d1259e20b994bca97e
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-01-18 16:37:16 -0800

    create endpoint to get all products and pagination

---
+++ app/api/products/route.ts
@@ -0,0 +1,39 @@
+import { NextResponse } from "next/server";
+import prisma from "@/libs/prisma";
+
+/**
+ * Retrieves a list of products based on the provided search criteria and page number.
+ *
+ * @param req - The request object containing the search criteria and page number.
+ * @returns A JSON response containing the list of products that match the search criteria.
+ * @throws If an error occurs while retrieving the products.
+ */
+export async function POST(req: Request){
+  const { search , page } = await req.json();
+  const productsPerPage = 8;
+
+  try {
+    const products = await prisma.products.findMany({
+      where: {
+        OR: [
+          { name: { contains: search } },
+          { id: { contains: search } }
+        ]
+      },
+      include: {
+        size: {},
+        category: {}
+      },
+      take: productsPerPage,
+      skip: (page - 1) * productsPerPage
+    });
+
+    return NextResponse.json(products)
+  } catch (error) {
+    return NextResponse.json({
+      message: "Ocurrió un error al obtener los productos",
+      error: error
+    }, { status: 500 })
+  }
+  // return Response.json('Obteniendo productos')
+}
