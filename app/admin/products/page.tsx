cdde968f9c614666a10593660c672168d3c93922
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-01-19 09:20:32 -0800

    change fetching products data

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,36 +1,65 @@
+'use client'
+
 import ProductsTable from '@/components/admin/products/ProductTable'
 import Pagination from '@/components/pagination/Pagination'
 import SearchBar from '@/components/search/SearchBar'
 import { ButtonLink } from '@/components/ui'
-import { getProducts, totalProducts } from '@/libs/products/fetching'
-import { ProductProps } from '@/types/product'
+import { ProductResponse } from '@/types/product'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
+import { useEffect, useState } from 'react'
 
-export default async function ProductsAdmin({
-  searchParams
-}: {
-  searchParams: { search: string, page: number} }
-) {
-  const search = searchParams?.search || '';
+export default function ProductsAdmin({ searchParams }: { searchParams: { product: string, page: number} }) {
+  const search = searchParams?.product || '';
   const page = searchParams?.page || 1;
-  const productsCount = await totalProducts();
-  const products = await getProducts(search, page);
+    const [products, setProducts] = useState<ProductResponse[]>([])
+  const [count, setCount] = useState<number>(0)
+
+  const getProducts = async (search: string, page: number) => {
+    try {
+      const response = await fetch('/api/products', {
+        method: "POST",
+        body: JSON.stringify({search,page})
+      })
+
+      const products = await response.json();
+      setProducts(products)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const getProductCount = async () => {
+    try {
+      const res = await fetch('/api/products', {
+        method: 'GET'
+      })
+
+      const count = await res.json();
+      setCount(count)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect( () => {
+    getProducts(search, page)
+    getProductCount()
+  },[page, search])
 
 
   return (
     <article className='flex flex-col gap-4'>
       <div className='flex justify-between py-3 px-6'>
-        <SearchBar placeholder='Buscar un producto' />
+        <SearchBar placeholder='Buscar un producto' query='product' />
         <ButtonLink href='/admin/products/new' className='flex items-center gap-2 font-bold bg-rose-100/80 hover:bg-rose-100 text-white px-3 py-2 rounded-md'>
           <FontAwesomeIcon icon={faPlus} className='w-4 h-4' />
           Agregar producto
         </ButtonLink>
       </div>
       <div className='flex flex-col justify-between bg-white rounded-lg shadow-xl min-h-[75vh] p-4'>
-        <ProductsTable title='Listado de productos' tableData={products as ProductProps[] } />
-        <Pagination count={productsCount} />
+        <ProductsTable title='Listado de productos' tableData={products} />
+        <Pagination count={count} />
       </div>
     </article>
   )
