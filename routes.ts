b8b27a2eaf35d2ae76579f5c6403cb3c9b0a5282
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-01-20 00:05:13 -0800

    create routes constants

---
+++ routes.ts
@@ -0,0 +1,33 @@
+/**
+ * An array of routes that are public.
+ * These routes don't require authentication.
+ * @type {string[]}
+ */
+export const publicRoutes = [
+  "/",
+  "/gallery",
+  "/contact",
+  "/shop"
+]
+
+/**
+ * An array of routes to authentication
+ * @type {string[]}
+ */
+export const authRoutes = [
+  "/auth/login",
+  "/auth/register",
+]
+
+/**
+ * The prefix for API authentication routes
+ * Routes that start with this prefix are used fro API authentication purposes.
+ * @type {string}
+ */
+export const apiAuthPrefix = "/api/auth";
+
+/**
+ * The default redirect after login
+ * @type {string}
+ */
+export const DEFAULT_LOGIN_LOGIN_REDIRECT = '/'
