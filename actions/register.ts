a178ca4a5340db3a76a5d813662612feb63e2780
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-01-19 20:44:47 -0800

    add register logic

--- actions/register.ts
+++ actions/register.ts
@@ -1,15 +1,36 @@
 'use server'
 
-import * as z from 'zod'
+import * as z from 'zod';
+import bcrypt from 'bcrypt';
+import prisma from '@/libs/prisma';
 import { RegisterSchema } from '@/schemas';
+import { getUserByEmail } from '@/data/user';
 
 export const register = async (data: z.infer<typeof RegisterSchema>) => {
-  console.log(data);
   const validateFields = RegisterSchema.safeParse(data);
 
   if (!validateFields.success) {
     return { error: "Ocurrió un error" };
   }
 
-  return { success: "Correo enviado!" };
+  const { name, email, password } = validateFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if(existingUser) {
+    return { error: "El correo ya está registrado" };
+  }
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword
+    },
+  })
+
+  // TODO Verification Email user creation
+
+  return { success: "Registro exitoso, revisa tu bandeja de entrada y confirma tu correo" };
 }
