92484d18926248a2a7f7752fb146dc9a57cb20d1
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-01-19 20:05:45 -0800

    add zod schemas to login and register

---
+++ schemas/index.ts
@@ -0,0 +1,33 @@
+import * as z from 'zod';
+
+export const LoginSchema = z.object({
+  email: z.string()
+  .email({
+    message: "Correo invalido"
+  }),
+  password: z.string()
+  .min(1, {
+    message: "Ingresa la contraseña"
+  })
+
+});
+
+export const RegisterSchema = z.object({
+  name: z.string({
+    required_error: "Este campo es requerido"
+  })
+  .min(3, {
+    message: "Ingresa un nombre valido"
+  })
+  .toLowerCase(),
+  email: z.string({
+    required_error: "Este campo es requerido"
+  })
+  .email({
+    message: "Correo invalido"
+  }),
+  password: z.string()
+  .min(6, {
+    message: "La contraseña debe contener al menos 6 caracteres"
+  })
+})
