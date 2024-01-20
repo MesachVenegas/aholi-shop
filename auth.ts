783b864195f262e3db0e4db149b6b3c474b2c12f
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-01-20 02:18:47 -0800

    add linked OAuth account validation

--- auth.ts
+++ auth.ts
@@ -13,14 +13,35 @@ export const {
   signIn,
   signOut
 } = NextAuth({
+  pages: {
+    signIn: "/auth/login",
+    error: "/auth/error",
+  },
+  events: {
+    async linkAccount({ user }) {
+      await prisma.user.update({
+        where: {
+          id: user.id
+        },
+        data: {
+          emailVerified: new Date()
+        }
+      })
+    }
+  },
   callbacks: {
+    // async signIn({ user }){
+    //   const existingUser = await getUserById(user.id as string);
+    //   if(!existingUser || !existingUser.emailVerified){
+    //     return false;
+    //   }
+    // },
     async session({ session, token }) {
-      console.log(session);
-      if(token.sub && session.user){
+      if (token.sub && session.user) {
         session.user.id = token.sub;
       }
 
-      if(token.role && session.user){
+      if (token.role && session.user) {
         session.user.role = token.role;
       }
 
@@ -28,11 +49,11 @@ export const {
     },
     async jwt({ token }) {
       // ? if token don't have sub return token;
-      if(!token.sub) return token;
+      if (!token.sub) return token;
       // ? if token have sub, check if user exists
       const existingUser = await getUserById(token.sub);
       //? If user exists, return token with role
-      if(!existingUser) return null;
+      if (!existingUser) return null;
       token.role = existingUser.role;
 
       return token;
