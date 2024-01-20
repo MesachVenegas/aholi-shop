a49c7e5f5ebbf5db2d2a557636b2119427044ded
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-01-20 00:10:07 -0800

    create authentication provider config

---
+++ auth.config.ts
@@ -0,0 +1,38 @@
+
+import type { NextAuthConfig } from "next-auth";
+
+import bcrypt from "bcryptjs";
+import Google from "next-auth/providers/google";
+import Credentials from 'next-auth/providers/credentials';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { LoginSchema } from "@/schemas";
+import { getUserByEmail } from "./data/user";
+
+export default {
+    providers: [
+        // Google({
+        //   clientId: process.env.GOOGLE_ID,
+        //   clientSecret: process.env.GOOGLE_SECRET,
+        // })
+        Credentials({
+            async authorize(credentials) {
+                // ? Validate form fields
+                const validateFields = LoginSchema.safeParse(credentials);
+                // ? if are valids
+                if (validateFields.success) {
+                    const { email, password } = validateFields.data;
+                    // ? Check if user exists
+                    const user = await getUserByEmail(email);
+                    if (!user || !user.password) return null;
+                    // ? Check if password match
+                    const passwordMatch = await bcrypt.compare(
+                        password,
+                        user.password
+                    );
+                    //? If match, return user
+                    if (passwordMatch) return user;
+                }
+            }
+        })
+    ]
+} satisfies NextAuthConfig;
