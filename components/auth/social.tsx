59365ac13081a91f612092ece0d2a4e6c4a07629
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-01-20 02:19:49 -0800

    add login provider handler

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -1,17 +1,19 @@
-import Image from "next/image";
 import { signIn } from "next-auth/react";
 import { FcGoogle } from 'react-icons/fc';
+import { DEFAULT_LOGIN_LOGIN_REDIRECT } from "@/routes";
 
 const Social = () => {
+  const oauthAuthentication = (provider: "google" | "facebook") => {
+    signIn(provider, {
+      callbackUrl: DEFAULT_LOGIN_LOGIN_REDIRECT
+    })
+  }
+
   return (
     <>
       <button
         className="flex gap-2 w-full max-w-[200px] text-sm md:text-base h-12 items-center justify-center rounded-lg border border-slate-300 shadow-lg hover:bg-slate-100 font-semibold"
-        onClick={async () => {
-          await signIn('google',{
-            callbackUrl: '/'
-          })
-        }}
+        onClick={() => oauthAuthentication("google")}
       >
         Iniciar sesion
         <FcGoogle className="w-7 h-7" />
