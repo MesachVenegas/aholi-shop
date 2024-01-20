dab4e96b2d518474b6d6f1fdd116c6c21142703f
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-01-20 02:19:20 -0800

    create card error component

---
+++ components/auth/error-card.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { faTriangleExclamation } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import Link from "next/link"
+
+const ErrorCard = () => {
+  return (
+    <div className="h-screen w-screen flex flex-col items-center justify-center gap-4">
+      <FontAwesomeIcon icon={faTriangleExclamation} className="w-48 h-48 text-red-500" />
+      <h1 className="text-5xl md:text-8xl font-bold">Oops! Algo salio mal!</h1>
+      <Link href="/auth/login" className="mt-6 bg-rose-700/80 text-white px-4 py-2 rounded-md hover:bg-rose-700">
+        Regresar
+      </Link>
+    </div>
+  )
+}
+
+export default ErrorCard
