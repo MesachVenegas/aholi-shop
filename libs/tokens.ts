700253bae74a7647e27b4ab96d5cda814ad31a14
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-01-20 02:43:58 -0800

    add generator token generator lib

---
+++ libs/tokens.ts
@@ -0,0 +1,33 @@
+import { getVerificationTokenByEmail } from '@/data/verication-token';
+import { v4 as uuidV4 } from 'uuid';
+import prisma from '@/libs/prisma';
+
+/**
+ * Generates a verification token for the given email.
+ *
+ * @param {string} email - The email for which the verification token is generated.
+ * @returns {Promise<object>} - A promise that resolves to the generated verification token.
+ */
+export const generateVerificationToken = async (email: string) => {
+  const token = uuidV4();
+  // expires token in 1 hour
+  const expires = new Date(new Date().getTime() + 3600 * 1000)
+
+  const existingToken = await getVerificationTokenByEmail(email);
+
+  if (existingToken) {
+    await prisma.verificationToken.delete({
+      where: { id: existingToken.id }
+    })
+  }
+
+  const verificationToken = await prisma.verificationToken.create({
+    data: {
+      email,
+      token,
+      expires
+    }
+  })
+
+  return verificationToken;
+}
