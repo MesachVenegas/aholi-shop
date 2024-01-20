b5a633ccead4f8623a0cf31403041c8602baaaf6
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-01-20 00:09:05 -0800

    add acthenticate login logic

--- actions/login.ts
+++ actions/login.ts
@@ -1,15 +1,37 @@
 'use server'
 
-import * as z from 'zod'
+import * as z from 'zod';
+import { AuthError } from 'next-auth';
+import { signIn } from '@/auth';
 import { LoginSchema } from '@/schemas';
+import { DEFAULT_LOGIN_LOGIN_REDIRECT } from '@/routes';
 
 export const login = async (data: z.infer<typeof LoginSchema>) => {
-  console.log(data);
   const validateFields = LoginSchema.safeParse(data);
 
   if(!validateFields.success) {
     return { error: "Campos Inválidos" };
   }
 
-  return { success: "Autentificación exitosa" };
+  const { email, password } = validateFields.data;
+
+  try{
+    await signIn('credentials', {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_LOGIN_REDIRECT,
+    })
+  } catch(error) {
+    if( error instanceof AuthError){
+      switch (error.type){
+        case "CredentialsSignin":
+          return { error: "Credenciales invalidas" };
+        default:
+          return { error: "Algo salio mal" };
+      }
+    }
+
+    throw error;
+  }
+
 }
