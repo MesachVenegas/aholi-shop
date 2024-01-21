9fed5039f773989b5ccd0c27d2f57f28075372ad
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-01-21 14:39:50 -0800

    change styles

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,9 +1,10 @@
 'use client'
 
+import { usePathname, useSearchParams, useRouter } from "next/navigation";
+
+import { useDebouncedCallback } from "use-debounce";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { usePathname, useSearchParams, useRouter } from "next/navigation";
-import { useDebouncedCallback } from "use-debounce";
 
 
 export default function SearchBar({ placeholder, query }: { placeholder: string, query: string }) {
@@ -25,14 +26,14 @@ export default function SearchBar({ placeholder, query }: { placeholder: string,
 
 
   return (
-    <div className="flex gap-2 items-center w-full max-w-md bg-white p-2 rounded-md">
-      <FontAwesomeIcon icon={faSearch}  className='w-4 h-4'/>
+    <div className="flex gap-2 items-center w-full max-w-xs sm:max-w-md bg-white px-2 rounded-md">
+      <FontAwesomeIcon icon={faSearch}  className='w-5 h-5 ml-2'/>
       <input
         name="search"
         type="search"
         placeholder={placeholder}
         onChange={ (e) => handleSearch(e.target.value) }
-        className="flex w-full h-full p-1 border-none focus:outline-none"
+        className="flex w-full h-full px-2 border-none focus:outline-none"
       />
     </div>
   )
