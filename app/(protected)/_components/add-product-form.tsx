934096ae34197dc6f50fa08479bb14ea942b27af
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-01-21 15:57:05 -0800

    add handler to create product

--- app/(protected)/_components/add-product-form.tsx
+++ app/(protected)/_components/add-product-form.tsx
@@ -27,6 +27,9 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/Input";
 import { Textarea } from "@/components/ui/textarea";
 import { SizesProps } from "@/types/size";
+import ToastNotification from "@/components/toast-notification";
+import { toast } from "react-toastify";
+import { addProduct } from "@/actions/products";
 
 
 
@@ -44,138 +47,38 @@ const AddProductForm = ( { categories, sizes } : { categories: CategoryProps[], 
     }
   });
 
-  const onSubmit = (data: z.infer<typeof AddProductSchema>) => {
-    console.log(data);
-
+  const onSubmit =  (data: z.infer<typeof AddProductSchema>) => {
+    addProduct(data)
+    .then( res => {
+      return;
+    })
+    .catch( error => {
+      toast.error(`${error}`)
+    })
   }
 
   return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="flex flex-col w-full max-w-xl h-full gap-2 space-y-4"
-      >
-        <div className="space-y-4">
-          <FormField
-            control={form.control}
-            name="name"
-            render={({field}) => (
-              <FormItem>
-                <FormLabel>Nombre</FormLabel>
-                <FormControl>
-                  <Input
-                    {...field}
-                    disabled={false}
-                    placeholder="Nombre del producto"
-                    className="bg-slate-200"
-                    type='text'
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          >
-          </FormField>
-        </div>
-
-        <div className="space-y-4">
-          <FormField
-            control={form.control}
-            name="description"
-            render={({field}) => (
-              <FormItem>
-                <FormLabel>Descripcion</FormLabel>
-                <FormControl>
-                  <Textarea
-                    {...field}
-                    disabled={false}
-                    placeholder="Descripcion del producto"
-                    className="bg-slate-200"
-                    rows={4}
-                    maxLength={180}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          >
-          </FormField>
-        </div>
-
-        <div className="flex w-full flex-col sm:flex-row justify-between gap-2">
-          <div>
-            <FormField
-              control={form.control}
-              name="categoryId"
-              render={({field}) => (
-                <FormItem>
-                  <FormLabel>Categoria</FormLabel>
-                  <Select onValueChange={field.onChange} required>
-                    <FormControl>
-                      <SelectTrigger className="bg-slate-200">
-                        <SelectValue placeholder="Selecciona una categoria" />
-                      </SelectTrigger>
-                    </FormControl>
-                    <SelectContent>
-                      {
-                        categories.map( category => (
-                          <SelectItem key={category?.id} value={`${category?.id}`}>{category?.name}</SelectItem>
-                        ))
-                      }
-                    </SelectContent>
-                  </Select>
-                  <FormMessage />
-                </FormItem>
-              )}
-            >
-            </FormField>
-          </div>
-          <div>
-            <FormField
-              control={form.control}
-              name="sizeId"
-              render={({field}) => (
-                <FormItem>
-                  <FormLabel>Tamaño</FormLabel>
-                  <Select onValueChange={field.onChange} required>
-                    <FormControl>
-                      <SelectTrigger className="bg-slate-200">
-                        <SelectValue placeholder="Selecciona una tamaño" />
-                      </SelectTrigger>
-                    </FormControl>
-                    <SelectContent>
-                      {
-                        sizes.map( size => (
-                          <SelectItem key={size?.id} value={`${size?.id}`}>
-                            {`${size?.name} (${size?.width}x${size?.height} cms)`}
-                          </SelectItem>
-                        ))
-                      }
-                    </SelectContent>
-                  </Select>
-                  <FormMessage />
-                </FormItem>
-              )}
-            >
-            </FormField>
-          </div>
-        </div>
-
-        <div className="flex w-full flex-col sm:flex-row justify-between gap-2">
+    <>
+      <ToastNotification />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col w-full max-w-xl h-full gap-2 space-y-4"
+        >
           <div className="space-y-4">
             <FormField
               control={form.control}
-              name="price"
+              name="name"
               render={({field}) => (
                 <FormItem>
-                  <FormLabel>Precio</FormLabel>
+                  <FormLabel>Nombre</FormLabel>
                   <FormControl>
                     <Input
                       {...field}
                       disabled={false}
-                      placeholder="17.45"
+                      placeholder="Nombre del producto"
                       className="bg-slate-200"
-                      type="number"
+                      type='text'
                     />
                   </FormControl>
                   <FormMessage />
@@ -184,19 +87,22 @@ const AddProductForm = ( { categories, sizes } : { categories: CategoryProps[], 
             >
             </FormField>
           </div>
-          {/* <div className="space-y-4">
+
+          <div className="space-y-4">
             <FormField
               control={form.control}
-              name="images"
+              name="description"
               render={({field}) => (
                 <FormItem>
-                  <FormLabel>Imágenes</FormLabel>
+                  <FormLabel>Descripcion</FormLabel>
                   <FormControl>
-                    <Input
+                    <Textarea
                       {...field}
                       disabled={false}
-                      multiple
-                      type="file"
+                      placeholder="Descripcion del producto"
+                      className="bg-slate-200"
+                      rows={4}
+                      maxLength={180}
                     />
                   </FormControl>
                   <FormMessage />
@@ -204,27 +110,132 @@ const AddProductForm = ( { categories, sizes } : { categories: CategoryProps[], 
               )}
             >
             </FormField>
-          </div> */}
-        </div>
+          </div>
 
-        <div className="flex w-full justify-between py-2">
-          <Button
-            className="bg-red-500/80 hover:bg-red-500"
-            type="reset"
-            onClick={ () => router.back()}
-          >
-            Cancelar
-          </Button>
-          <Button
-            className="bg-rose-100/80 hover:bg-rose-100"
-            type="submit"
-          >
-            Agregar Producto
-          </Button>
-        </div>
+          <div className="flex w-full flex-col sm:flex-row justify-between gap-2">
+            <div>
+              <FormField
+                control={form.control}
+                name="categoryId"
+                render={({field}) => (
+                  <FormItem>
+                    <FormLabel>Categoria</FormLabel>
+                    <Select onValueChange={field.onChange} required>
+                      <FormControl>
+                        <SelectTrigger className="bg-slate-200">
+                          <SelectValue placeholder="Selecciona una categoria" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {
+                          categories.map( category => (
+                            <SelectItem key={category?.id} value={`${category?.id}`}>{category?.name}</SelectItem>
+                          ))
+                        }
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              >
+              </FormField>
+            </div>
+            <div>
+              <FormField
+                control={form.control}
+                name="sizeId"
+                render={({field}) => (
+                  <FormItem>
+                    <FormLabel>Tamaño</FormLabel>
+                    <Select onValueChange={field.onChange} required>
+                      <FormControl>
+                        <SelectTrigger className="bg-slate-200">
+                          <SelectValue placeholder="Selecciona una tamaño" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {
+                          sizes.map( size => (
+                            <SelectItem key={size?.id} value={`${size?.id}`}>
+                              {`${size?.name} (${size?.width}x${size?.height} cms)`}
+                            </SelectItem>
+                          ))
+                        }
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              >
+              </FormField>
+            </div>
+          </div>
 
-      </form>
-    </Form>
+          <div className="flex w-full flex-col sm:flex-row justify-between gap-2">
+            <div className="space-y-4">
+              <FormField
+                control={form.control}
+                name="price"
+                render={({field}) => (
+                  <FormItem>
+                    <FormLabel>Precio</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        disabled={false}
+                        placeholder="17.45"
+                        className="bg-slate-200"
+                        type="number"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              >
+              </FormField>
+            </div>
+            {/* <div className="space-y-4">
+              <FormField
+                control={form.control}
+                name="images"
+                render={({field}) => (
+                  <FormItem>
+                    <FormLabel>Imágenes</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        disabled={false}
+                        multiple
+                        type="file"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              >
+              </FormField>
+            </div> */}
+          </div>
+
+          <div className="flex w-full justify-between py-2">
+            <Button
+              className="bg-red-500/80 hover:bg-red-500"
+              type="reset"
+              onClick={ () => router.back()}
+            >
+              Cancelar
+            </Button>
+            <Button
+              className="bg-rose-100/80 hover:bg-rose-100"
+              type="submit"
+            >
+              Agregar Producto
+            </Button>
+          </div>
+
+        </form>
+      </Form>
+    </>
   )
 }
 
