d952fa70da97cec3f353d2ebbe5c9a2aef37940d
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-01-20 22:28:15 -0800

    create reset password page

---
+++ app/auth/reset-password/page.tsx
@@ -0,0 +1,11 @@
+import SetPasswordForm from "@/components/auth/set-password-form"
+
+const NewPassordPage = () => {
+  return (
+    <div className="flex justify-center items-center p-10">
+      <SetPasswordForm />
+    </div>
+  )
+}
+
+export default NewPassordPage
