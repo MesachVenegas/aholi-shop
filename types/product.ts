072d656b027d57a4a2e838e63dcaca41c0534efa
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-01-21 14:34:54 -0800

    change products response data base response

--- types/product.ts
+++ types/product.ts
@@ -1,84 +1,22 @@
-export interface ProductProps {
+export type ProductProps = {
   id: string;
   name: string;
   price: number;
+  sizeId: number;
+  categoryId: number;
   description: string;
+  images: string;
   size: {
     id: number;
     name: string;
     width: string;
     height: string;
     type: string;
-  };
+  }
   category: {
     id: number;
     name: string;
     icon: string;
-  };
-  images: string;
-};
-
-export interface CategoriesProps {
-  id: number;
-  name: string;
-  description?: string;
-  icon?: string | null;
-}
-
-export interface SizesProps {
-  id: number;
-  name: string;
-  width: string;
-  height: string;
-  type: string;
-}
-
-export interface ImagesProps {
-  index?: number;
-  id? : number;
-  name: string;
-  url: string;
-  file: {
-    lastModified: number;
-    lastModifiedDate: Date;
-    name: string;
-    size: number;
-    type: string;
-    webkitRelativePath: string;
-  }
-}
-
-export type ProductFormPros = {
-  name: string;
-  price: string;
-  sizes: string;
-  category: string;
-  description: string;
-  images: string | Blob;
-}
-
-
-export type ProductResponse = {
-  id: string;
-  name: string;
-  description: string;
-  price: number;
-  images: {
-    name: string;
-    url: string;
-    alt: string;
-  }[];
-  size: {
-    id: number;
-    name: string;
-    width: string;
-    height: string;
-    type: string;
-  }
-  category: {
-    id: number;
-    name: string;
     description: string;
-    icon?: string;
   }
-}
+} | undefined
