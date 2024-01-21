145541fb0f0d0c5154b8f1baa1d361b4a10d1d84
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-01-21 15:57:29 -0800

    add toast notification on delete product and handle delete product

--- app/(protected)/_components/ProductTable.tsx
+++ app/(protected)/_components/ProductTable.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { toast } from "react-toastify";
 import {
   Card,
   Table,
@@ -18,6 +19,9 @@ import {
 
 import { valueFormatter } from "@/libs/utils";
 import { ProductProps } from "@/types/product";
+import ToastNotification from "@/components/toast-notification";
+import { deleteProduct } from "@/actions/products";
+import { error } from "console";
 
 
 export default function ProductsTable ({ data } : { data: ProductProps[]}){
@@ -32,55 +36,71 @@ export default function ProductsTable ({ data } : { data: ProductProps[]}){
           height={360}
           alt="empty products"
         />
-        <h2 className="text-2xl">No hay productos disponibles agrega un producto.</h2>
+        <h2 className="text-2xl">Sin productos :(</h2>
       </div>
     )
   }
 
+  const handleDelete = (id: string) => {
+    deleteProduct(id)
+      .then( res => {
+        toast.success(`${res.success}`)
+      })
+      .catch( error => {
+        toast.error(`${error}`)
+      })
+  }
+
   return (
-    <Card className="flex flex-col w-full h-full overflow-auto">
-      <Title className="text-xl">Productos</Title>
-      <Table className="mt-5">
-        <TableHead>
-          <TableRow className="bg-rose-100/20">
-            <TableHeaderCell>Nombre</TableHeaderCell>
-            <TableHeaderCell>Descripcion</TableHeaderCell>
-            <TableHeaderCell>Medidas</TableHeaderCell>
-            <TableHeaderCell>Categorías</TableHeaderCell>
-            <TableHeaderCell>Precio</TableHeaderCell>
-            <TableHeaderCell></TableHeaderCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.map((prod: ProductProps) => (
-            <TableRow key={prod?.id} className="hover:bg-rose-100/10">
-              <TableCell>{prod?.name}</TableCell>
-              <TableCell>
-                <Text>{prod?.description}</Text>
-              </TableCell>
-              <TableCell>
-                <Text>{prod?.size.height}cms x {prod?.size.width}cms</Text>
-              </TableCell>
-              <TableCell className="flex gap-2">
-                {prod?.category.name}
-              </TableCell>
-              <TableCell>
-                  {valueFormatter(prod?.price as unknown as number)}
-              </TableCell>
-              <TableCell className="flex gap-3">
-                  <span className="flex items-center gap-1 cursor-pointer text-rose-100">
-                    <FontAwesomeIcon icon={faEdit} className="w-4 h-4" />
-                    Editar
-                  </span>
-                  <span className="flex items-center gap-1 cursor-pointer text-red-600">
-                    <FontAwesomeIcon icon={faTrash} className="w-4 h-4" />
-                    Eliminar
-                  </span>
-              </TableCell>
+    <>
+      <Card className="flex flex-col w-full h-full overflow-auto">
+        <Title className="text-xl">Productos</Title>
+        <Table className="mt-5">
+          <TableHead>
+            <TableRow className="bg-rose-100/20">
+              <TableHeaderCell>Nombre</TableHeaderCell>
+              <TableHeaderCell>Descripcion</TableHeaderCell>
+              <TableHeaderCell>Medidas</TableHeaderCell>
+              <TableHeaderCell>Categoría</TableHeaderCell>
+              <TableHeaderCell>Precio</TableHeaderCell>
+              <TableHeaderCell></TableHeaderCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </Card>
+          </TableHead>
+          <TableBody>
+            {data.map((prod: ProductProps) => (
+              <TableRow key={prod?.id} className="hover:bg-rose-100/10">
+                <TableCell className="text-center">{prod?.name}</TableCell>
+                <TableCell className="text-center">
+                  <Text>{prod?.description}</Text>
+                </TableCell>
+                <TableCell className="text-center">
+                  <Text>{prod?.size.height}cms x {prod?.size.width}cms</Text>
+                </TableCell >
+                <TableCell className="text-center">
+                  {prod?.category.name}
+                </TableCell>
+                <TableCell className="text-center">
+                    {valueFormatter(prod?.price as unknown as number)}
+                </TableCell>
+                <TableCell className="flex gap-3">
+                    <span className="flex items-center gap-1 cursor-pointer text-rose-100">
+                      <FontAwesomeIcon icon={faEdit} className="w-4 h-4" />
+                      Editar
+                    </span>
+                    <span
+                      className="flex items-center gap-1 cursor-pointer text-red-600"
+                      onClick={ () => handleDelete(prod?.id as string)}
+                    >
+                      <FontAwesomeIcon icon={faTrash} className="w-4 h-4" />
+                      Eliminar
+                    </span>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Card>
+      <ToastNotification />
+    </>
   );
 }
