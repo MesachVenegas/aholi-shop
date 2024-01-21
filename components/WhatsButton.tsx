8d823f10b884a72ed4a85f0428f531fd3248b819
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-01-21 14:39:23 -0800

    change sizes on desktop view

--- components/WhatsButton.tsx
+++ components/WhatsButton.tsx
@@ -9,13 +9,19 @@ export default function WhatsButton() {
   const pathname = usePathname();
 
 
-  if(pathname !== '/admin' && pathname !== '/admin/products' && pathname !== '/admin/tags' && pathname !== '/admin/gallery'){
+  if(
+      pathname !== '/admin'
+      && pathname !== '/admin/products'
+      && pathname !== '/admin/tags'
+      && pathname !== '/admin/gallery'
+      && pathname !== '/admin/products/new'
+    ){
     return (
         <Link
           href='https://api.whatsapp.com/send?phone=523321860284'
-          className='flex justify-center items-center fixed bg-rose-700 border border-rose-100 w-12 h-12 lg:w-20 lg:h-20 rounded-full z-50 right-4 bottom-1/2 shadow-xl'
+          className='flex justify-center items-center fixed bg-rose-700 border border-rose-100 w-12 h-12 lg:w-16 lg:h-16 rounded-full z-50 right-4 bottom-1/2 shadow-xl'
         >
-          <FontAwesomeIcon icon={faWhatsapp} className='w-8 h-8 lg:w-12 lg:h-12 text-white' />
+          <FontAwesomeIcon icon={faWhatsapp} className='w-8 h-8 lg:w-10 lg:h-10 text-white' />
         </Link>
     )
   }
