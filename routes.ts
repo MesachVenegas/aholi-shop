89e3e84fc521190b1ca1fe690c093fff088ef9d1
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-01-21 17:10:55 -0800

    update default_url

--- routes.ts
+++ routes.ts
@@ -34,4 +34,4 @@ export const apiAuthPrefix = "/api/auth";
  * The default redirect after login
  * @type {string}
  */
-export const DEFAULT_LOGIN_LOGIN_REDIRECT = '/'
+export const DEFAULT_LOGIN_LOGIN_REDIRECT = 'www.aholi.shop'
