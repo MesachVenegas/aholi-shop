4752f56a0ff5e0b1c9584dbb43caa81f0e72e557
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-01-22 17:08:51 -0800

    change component display

--- components/WhatsButton.tsx
+++ components/WhatsButton.tsx
@@ -1,29 +1,18 @@
 'use client'
 
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
 import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function WhatsButton() {
-  const pathname = usePathname();
 
-
-  if(
-      pathname !== '/admin'
-      && pathname !== '/admin/products'
-      && pathname !== '/admin/tags'
-      && pathname !== '/admin/gallery'
-      && pathname !== '/admin/products/new'
-    ){
-    return (
-        <Link
-          href='https://api.whatsapp.com/send?phone=523321860284'
-          className='flex justify-center items-center fixed bg-rose-700 border border-rose-100 w-12 h-12 lg:w-16 lg:h-16 rounded-full z-50 right-4 bottom-1/2 shadow-xl'
-        >
-          <FontAwesomeIcon icon={faWhatsapp} className='w-8 h-8 lg:w-10 lg:h-10 text-white' />
-        </Link>
-    )
-  }
+  return (
+      <Link
+        href='https://api.whatsapp.com/send?phone=523321860284'
+        className='flex justify-center items-center fixed bg-rose-700 border border-rose-100 w-12 h-12 lg:w-16 lg:h-16 rounded-full z-50 right-4 bottom-1/2 shadow-xl'
+      >
+        <FontAwesomeIcon icon={faWhatsapp} className='w-8 h-8 lg:w-10 lg:h-10 text-white' />
+      </Link>
+  )
 
 }
