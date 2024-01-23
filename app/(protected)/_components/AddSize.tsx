52528823f3dfbb32bfcbc25c9b77f8423a8e52e8
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-01-23 01:23:37 -0800

    change add size handle form

--- app/(protected)/_components/AddSize.tsx
+++ app/(protected)/_components/AddSize.tsx
@@ -1,26 +1,50 @@
 'use client'
 
 import { useState } from 'react'
-import { faAdd, faXmark } from '@fortawesome/free-solid-svg-icons';
+
+import * as z from 'zod';
+import { toast } from 'react-toastify';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { faAdd } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { newSize } from '@/libs/sizes/actions';
+
+import { recursive } from '@/styles/fonts';
+import { AddNewSize } from '@/actions/sizes';
+import { AddNewSizeSchema } from '@/schemas';
+import { Input } from '@/components/ui/Input';
+import { Button } from '@/components/ui/button';
+import ToastNotification from '@/components/toast-notification';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 
 export default function AddSize() {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const form = useForm<z.infer<typeof AddNewSizeSchema>>({
+    resolver: zodResolver(AddNewSizeSchema),
+    defaultValues: {
+      name: '',
+      height: '',
+      width: '',
+      type: ''
+    }
+  });
 
-  const addNewSize = async (data : Iterable<readonly [PropertyKey, any]>) => {
-    await newSize(data)
+  const handleSave = (data : z.infer<typeof AddNewSizeSchema>) => {
+    AddNewSize(data)
       .then( res => {
-        setIsOpen(false);
+        form.reset()
+        toast.success(res?.success)
       }).catch( error => {
-        console.error(error)
-      });
+        toast.error(error)
+      })
+      .finally( () => setIsOpen(false))
   }
 
 
   return (
     <div className="flex">
         <button
+          type='button'
           className="flex justify-center items-center gap-3 bg-rose-100/80 hover:bg-rose-100 px-3 rounded-lg text-white"
           onClick={() => setIsOpen(true)}
           >
@@ -29,84 +53,120 @@ export default function AddSize() {
         </button>
         {
           isOpen && (
-            <div className="fixed top-0 left-0 flex justify-center items-center w-full h-full backdrop-blur-md z-10">
-              <form
-                action={addNewSize}
-                className="flex flex-col w-full max-w-xl p-6 shadow-2xl bg-white gap-6 rounded-lg justify-center items-center"
-              >
-                <span className="flex self-end hover:bg-red-600 hover:text-white transition-all">
-                  <FontAwesomeIcon
-                    icon={faXmark}
-                    className="w-6 h-6 cursor-pointer"
-                    onClick={() => setIsOpen(false)}
-                  />
-                </span>
-                <h2 className="text-2xl font-medium">Agregar Nuevo Tamaño</h2>
-                <div className='flex flex-col gap-4'>
-                  <div>
-                    <label htmlFor="name">Nombre</label>
-                    <input
-                      id="name"
-                      type="text"
-                      name='name'
-                      className="bg-slate-200 w-full max-w-sm p-2 rounded-lg border border-transparent focus:outline-none focus:border-rose-700"
-                    />
-                  </div>
-                  <div className='flex items-center justify-between gap-4'>
-                    <label htmlFor="height">Alto</label>
-                    <div className='flex items-center gap-2'>
-                      <input
-                        id="height"
-                        type="text"
-                        name='height'
-                        className="bg-slate-200 w-full max-w-[150px] p-2 rounded-lg border border-transparent focus:outline-none focus:border-rose-700"
-                      />
-                      cms
-                    </div>
-                  </div>
-                  <div className='flex items-center justify-between gap-4'>
-                    <label htmlFor="width">Ancho</label>
-                    <div className='flex items-center gap-2'>
-                      <input
-                        id="width"
-                        type="text"
-                        name='width'
-                        className="bg-slate-200 w-full max-w-[150px] p-2 rounded-lg border border-transparent focus:outline-none focus:border-rose-700"
-                      />
-                      cms
-                    </div>
-                  </div>
-                  <div className='flex items-center justify-between gap-4'>
-                    <label htmlFor="type">Tipo</label>
-                    <select
-                      id="type"
-                      name='type'
-                      className="bg-slate-200 w-full max-w-[190px] p-2 rounded-lg border border-transparent focus:outline-none focus:border-rose-700"
+            <div className="fixed top-0 left-0 flex flex-col justify-center items-center w-full h-full backdrop-blur-md z-50">
+              <Form {...form}>
+                <form
+                  onSubmit={form.handleSubmit(handleSave)}
+                  className="flex flex-col justify-center items-center bg-white gap-4 w-full max-w-lg rounded-md m-auto p-10 shadow-lg"
+                >
+                  <h2 className={`${recursive.className} font-medium text-2xl`}>Agregar Nuevo Tamaño</h2>
+                  <div className="space-y-4 w-full">
+                    <FormField
+                      control={form.control}
+                      name="name"
+                      render={({field}) => (
+                        <FormItem>
+                          <FormLabel>Nombre</FormLabel>
+                          <FormControl>
+                            <Input
+                              {...field}
+                              placeholder="Nombre del producto"
+                              className="bg-slate-200"
+                              type='text'
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
                     >
-                      <option value="select">Seleccionar</option>
-                      <option value="rounded">Circular</option>
-                      <option value="hexagonal">Hexagonal</option>
-                      <option value="tube">Tubular</option>
-                      <option value="scale">Escalonada</option>
-                    </select>
+                    </FormField>
                   </div>
-                </div>
-                <div className="flex justify-around w-full p-2">
-                  <button
-                    type="reset"
-                    onClick={() => setIsOpen(false)}
-                    className="flex justify-center items-center py-2 px-3 bg-red-500/80 text-white rounded-lg font-bold hover:bg-red-500"
-                  >
-                    Cancelar
-                  </button>
-                  <button className="flex justify-center items-center py-2 px-3 bg-rose-100/80 text-white rounded-lg font-bold hover:bg-rose-100">
-                    Guardar
-                  </button>
-                </div>
-              </form>
+
+                  <div className='flex justify-between w-full gap-2'>
+                    <div className="space-y-4 w-full">
+                      <FormField
+                        control={form.control}
+                        name="height"
+                        render={({field}) => (
+                          <FormItem>
+                            <FormLabel>Altura</FormLabel>
+                            <FormControl>
+                              <Input
+                                {...field}
+                                placeholder="Altura en cms"
+                                className="bg-slate-200"
+                                type='number'
+                              />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      >
+                      </FormField>
+                    </div>
+                    <div className="space-y-4 w-full">
+                      <FormField
+                        control={form.control}
+                        name="width"
+                        render={({field}) => (
+                          <FormItem>
+                            <FormLabel>Ancho</FormLabel>
+                            <FormControl>
+                              <Input
+                                {...field}
+                                placeholder="Ancho en cms"
+                                className="bg-slate-200"
+                                type='number'
+                              />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      >
+                      </FormField>
+                    </div>
+                  </div>
+
+                  <div className="space-y-4 w-full">
+                    <FormField
+                      control={form.control}
+                      name="type"
+                      render={({field}) => (
+                        <FormItem>
+                          <FormLabel>Tipo/Forma</FormLabel>
+                          <FormControl>
+                            <Input
+                              {...field}
+                              placeholder="Hexagonal, circular ..."
+                              className="bg-slate-200"
+                              type='text'
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    >
+                    </FormField>
+                  </div>
+
+                  <div className="flex w-full justify-between py-2 mt-5">
+                    <Button
+                      className="bg-red-500/80 hover:bg-red-500"
+                      type="reset"
+                      onClick={ () => { setIsOpen(false) }}
+                    >
+                      Cancelar
+                    </Button>
+                    <Button className="bg-rose-100/80 hover:bg-rose-100" type="submit">
+                      Agregar
+                    </Button>
+                  </div>
+                </form>
+              </Form>
             </div>
           )
         }
-      </div>
+        <ToastNotification />
+    </div>
   )
 }
