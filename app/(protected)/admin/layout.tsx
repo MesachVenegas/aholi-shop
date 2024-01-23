f9cbfcea40520c74b61ff0a5b19d19c911941736
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-01-22 17:17:31 -0800

    add role gate to control acccess

--- app/(protected)/admin/layout.tsx
+++ app/(protected)/admin/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from 'next';
-import { redirect } from 'next/navigation';
 
 import { auth } from '@/auth';
 import { getUserByEmail } from '@/data/user';
 import SideBar from '@/app/(protected)/_components/SideBar';
+import RoleGate from '@/components/auth/role-gate';
+import { Role } from '@prisma/client';
 
 export const metadata: Metadata = {
   title: 'Panel de Administración',
@@ -13,15 +14,17 @@ export default async function ProtectedAdminLayout({ children }: { children: Rea
   const session = await auth();
   const user = await getUserByEmail(session?.user.email as string);
 
-  if(user?.role === 'user'){
-    redirect('/error')
-  }
+
   return (
-    <main className='flex flex-col lg:flex-row'>
-      <SideBar />
-      <div className='flex flex-col w-full overflow-auto py-10 xl:p-10'>
-        {children}
-      </div>
-    </main>
+    <>
+    <RoleGate allowedRole='admin' role={user?.role as Role} >
+      <main className='flex flex-col lg:flex-row'>
+        <SideBar />
+        <div className='flex flex-col w-full overflow-auto py-10 xl:p-10'>
+          {children}
+        </div>
+      </main>
+    </RoleGate>
+    </>
   )
 }
