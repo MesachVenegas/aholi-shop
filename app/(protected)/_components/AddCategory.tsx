8a3a7b5bb8e4ef21ca960b10a12f3f2809c78580
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-01-23 01:23:11 -0800

    style mobile view

--- app/(protected)/_components/AddCategory.tsx
+++ app/(protected)/_components/AddCategory.tsx
@@ -2,42 +2,35 @@
 
 import * as z from 'zod';
 
-import { useState, useTransition } from "react";
-import { faAdd, faXmark } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { AddCategorySchema } from "@/schemas";
-import { addNewCategory } from '@/actions/categories';
+import { useState } from "react";
 import { toast } from 'react-toastify';
-import { useRouter } from 'next/navigation';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { faAdd } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { recursive } from '@/styles/fonts';
 import { Input } from '@/components/ui/Input';
+import { AddCategorySchema } from "@/schemas";
 import { Button } from '@/components/ui/button';
 import { Textarea } from '@/components/ui/textarea';
-import { recursive } from '@/styles/fonts';
+import { addNewCategory } from '@/actions/categories';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 
 export default function AddCategory() {
-  const router =  useRouter();
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  const [isPending, startTransition] = useTransition();
-
   const form = useForm<z.infer<typeof AddCategorySchema>>({
     resolver: zodResolver(AddCategorySchema),
-    defaultValues: {
-      name: '',
-      description: '',
-    }
+    defaultValues: { name: '', description: '' }
   });
 
   const onSubmit =  (data: z.infer<typeof AddCategorySchema>) => {
     addNewCategory(data)
     .then( res => {
-      console.log(res);
-      if(res.error){
-        toast.error(res.error)
-      }
-      form.reset();
+      if(res.error) toast.error(res.error);
+
+      form.reset(data);
+
       toast.success(res.success)
     })
     .catch( error => {
@@ -58,14 +51,14 @@ export default function AddCategory() {
         </button>
         {
           isOpen && (
-            <div className="fixed top-0 left-0 flex justify-center items-center w-full h-full backdrop-blur-md z-50">
-                <h2 className={`${recursive.className} font-medium text-xl`}>Agregar Categoria</h2>
+            <div className="fixed top-0 left-0 flex flex-col justify-center items-center w-full h-full backdrop-blur-md z-50">
                 <Form {...form}>
                   <form
                     onSubmit={form.handleSubmit(onSubmit)}
-                    className="flex flex-col justify-center items-center bg-white w-full max-w-lg m-auto p-10 shadow-lg"
+                    className="flex flex-col justify-center items-center bg-white gap-4 w-full max-w-lg m-auto p-10 shadow-lg"
                   >
-                    <div className="space-y-4">
+                    <h2 className={`${recursive.className} font-medium text-2xl`}>Agregar Categoria</h2>
+                    <div className="space-y-4 w-full">
                       <FormField
                         control={form.control}
                         name="name"
@@ -75,7 +68,6 @@ export default function AddCategory() {
                             <FormControl>
                               <Input
                                 {...field}
-                                disabled={isPending}
                                 placeholder="Nombre del producto"
                                 className="bg-slate-200"
                                 type='text'
@@ -88,7 +80,7 @@ export default function AddCategory() {
                       </FormField>
                     </div>
 
-                    <div className="space-y-4">
+                    <div className="space-y-4 w-full">
                       <FormField
                         control={form.control}
                         name="description"
@@ -98,7 +90,6 @@ export default function AddCategory() {
                             <FormControl>
                               <Textarea
                                 {...field}
-                                disabled={isPending}
                                 placeholder="Descripcion del producto"
                                 className="bg-slate-200"
                                 rows={4}
@@ -112,7 +103,7 @@ export default function AddCategory() {
                       </FormField>
                     </div>
 
-                    <div className="flex w-full justify-between py-2">
+                    <div className="flex w-full justify-between py-2 mt-5">
                       <Button
                         className="bg-red-500/80 hover:bg-red-500"
                         type="reset"
