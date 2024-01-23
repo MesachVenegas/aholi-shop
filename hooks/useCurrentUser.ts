c33f9fc99fc27b1bdc636f8ae266442ef3bc37f4
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-01-22 17:07:01 -0800

    typo comment

--- hooks/useCurrentUser.ts
+++ hooks/useCurrentUser.ts
@@ -3,7 +3,6 @@ import { useSession } from "next-auth/react";;
 export const useCurrentUser = () => {
   const session = useSession();
 
-  // TODO: add role propertie and add to user object
 
   return session.data?.user
 }
