96dccb110775ff09f79bd148e28e90bfcd38d35e
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-01-22 17:12:51 -0800

    typo

--- app/(protected)/admin/tags/page.tsx
+++ app/(protected)/admin/tags/page.tsx
@@ -4,7 +4,6 @@ import CategorySection from '@/app/(protected)/_components/CategorySection'
 
 export default function CategoriesAdmin({ searchParams }: { searchParams: {category: string, page: number, size: string}}) {
 
-
   return (
     <div>
       <div className='flex flex-col xl:flex-row w-full gap-8 p-4'>
