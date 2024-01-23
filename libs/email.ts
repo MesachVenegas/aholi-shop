8c83deef966c8234c41e4072bd6ab81a4a40ecdc
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-01-23 02:36:43 -0800

    add function to handle contact form email

--- libs/email.ts
+++ libs/email.ts
@@ -1,7 +1,10 @@
 'use server'
 
-import { DOMAIN_LOGIN_REDIRECT } from '@/routes';
+import * as z from 'zod';
 import { Resend } from 'resend';
+
+import { DOMAIN_LOGIN_REDIRECT } from '@/routes';
+import { ContactEmailSchema } from '@/schemas';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -27,6 +30,13 @@ export const sendVerificationEmail = async (email: string, token: string) => {
   })
 }
 
+/**
+ * Sends a password reset email to the specified email address.
+ *
+ * @param email - The email address to send the password reset email to.
+ * @param token - The token used for password reset.
+ * @returns A promise that resolves when the email is sent successfully.
+ */
 export const sendPasswordResetEmail = async (email: string, token: string) => {
   let resetLink = `${DOMAIN_LOGIN_REDIRECT}/auth/reset-password?token=${token}`;
 
@@ -39,6 +49,48 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
     from: 'ventas@aholi.shop',
     to: email,
     subject: 'Restauracion de password',
-    html: `Ingresa al enlace para continuar con el proceso de restauracion: <a href="${resetLink}">Restaurar</a></>`
+    html: `
+      Ingresa al enlace para continuar con el proceso de restauracion: <a href="${resetLink}">Restaurar</a></>
+    `
+  })
+}
+
+
+/**
+ * Sends a contact email with the provided data.
+ *
+ * @param {object} data - The data for the contact email.
+ * @param {string} data.name - The name of the sender.
+ * @param {string} data.email - The email of the sender.
+ * @param {string} data.phone - The phone number of the sender.
+ * @param {string} data.message - The message from the sender.
+ * @throws {Error} If the data fields are invalid.
+ * @returns {Promise<void>} A promise that resolves when the email is sent.
+ */
+export const contactEmail = async (data: z.infer<typeof ContactEmailSchema>) => {
+  const isValidFields = ContactEmailSchema.safeParse(data);
+
+  if(!isValidFields.success) throw new Error("Campos inválidos");
+
+  const { name, email, phone, message } = data;
+
+  await resend.emails.send({
+    from: 'vetas@aholi.shop',
+    to: 'ventas@aholi.shop',
+    subject: "Un cliente quiere ponerse en contacto! 🎉",
+    html: `
+      <div>
+        <h1>Un cliente busca tus servicios</h1>
+        <p>Estos son sus datos</p>
+        <ul>
+          <li>Nombre: ${name}</li>
+          <li>Correo: ${email}</li>
+          <li>Teléfono: ${phone}</li>
+        </ul>
+        <div>
+          ${message}
+        </div>
+      </div>
+    `
   })
 }
