f143122dc0d346ebd82a8e737a130cad33959400
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-01-23 22:34:17 -0800

    add side menu

--- app/(protected)/_components/AddSize.tsx
+++ app/(protected)/_components/AddSize.tsx
@@ -1,65 +1,56 @@
 'use client'
 
-import { useState } from 'react'
+import { useState } from 'react';
 
 import * as z from 'zod';
 import { toast } from 'react-toastify';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { faAdd } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMaximize } from '@fortawesome/free-solid-svg-icons';
 
-import { recursive } from '@/styles/fonts';
 import { AddNewSize } from '@/actions/sizes';
 import { AddNewSizeSchema } from '@/schemas';
 import { Input } from '@/components/ui/Input';
 import { Button } from '@/components/ui/button';
-import ToastNotification from '@/components/toast-notification';
+import { SheetClose } from '@/components/ui/sheet';
+import AsideWrapper from '@/components/aside-menu_wrap';
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+
 
 export default function AddSize() {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+
   const form = useForm<z.infer<typeof AddNewSizeSchema>>({
     resolver: zodResolver(AddNewSizeSchema),
     defaultValues: {
-      name: '',
-      height: '',
-      width: '',
-      type: ''
+      name: undefined,
+      height: undefined,
+      width: undefined,
+      type: undefined
     }
   });
 
   const handleSave = (data : z.infer<typeof AddNewSizeSchema>) => {
     AddNewSize(data)
       .then( res => {
-        form.reset()
         toast.success(res?.success)
       }).catch( error => {
         toast.error(error)
       })
-      .finally( () => setIsOpen(false))
   }
 
 
   return (
     <div className="flex">
-        <button
-          type='button'
-          className="flex justify-center items-center gap-3 bg-rose-100/80 hover:bg-rose-100 px-3 rounded-lg text-white"
-          onClick={() => setIsOpen(true)}
-          >
-          <FontAwesomeIcon className="w-4 h-5" icon={faAdd} />
-          Nuevo Tamaño
-        </button>
-        {
-          isOpen && (
-            <div className="fixed top-0 left-0 flex flex-col justify-center items-center w-full h-full backdrop-blur-md z-50">
-              <Form {...form}>
-                <form
-                  onSubmit={form.handleSubmit(handleSave)}
-                  className="flex flex-col justify-center items-center bg-white gap-4 w-full max-w-lg rounded-md m-auto p-10 shadow-lg"
-                >
-                  <h2 className={`${recursive.className} font-medium text-2xl`}>Agregar Nuevo Tamaño</h2>
+        <AsideWrapper
+          title='Agregar Tamaño'
+          description='Aquí podrás agregar un nuevo tamaño rellena los campos con la información requerida y al finalizar no olvides guardar los cambios'
+          triggerText='Agregar'
+          styleTrigger='bg-rose-100/80 hover:bg-rose-100 rounded-lg text-white w-full max-w-[250px]'
+          icon={faMaximize}
+        >
+          <Form {...form}>
+                <form onSubmit={form.handleSubmit(handleSave)} >
                   <div className="space-y-4 w-full">
                     <FormField
                       control={form.control}
@@ -150,23 +141,18 @@ export default function AddSize() {
                   </div>
 
                   <div className="flex w-full justify-between py-2 mt-5">
-                    <Button
-                      className="bg-red-500/80 hover:bg-red-500"
-                      type="reset"
-                      onClick={ () => { setIsOpen(false) }}
-                    >
-                      Cancelar
-                    </Button>
+                    <SheetClose>
+                      <Button className="bg-red-500/80 hover:bg-red-500" type="reset" >
+                        Cancelar
+                      </Button>
+                    </SheetClose>
                     <Button className="bg-rose-100/80 hover:bg-rose-100" type="submit">
                       Agregar
                     </Button>
                   </div>
                 </form>
               </Form>
-            </div>
-          )
-        }
-        <ToastNotification />
+        </AsideWrapper>
     </div>
   )
 }
