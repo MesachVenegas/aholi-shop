7a8013bdd2cf89af0a1b472942c3d18a8a06913c
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-01-23 22:35:27 -0800

    change style

--- app/(protected)/_components/CategoriesTable.tsx
+++ app/(protected)/_components/CategoriesTable.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { toast } from "react-toastify";
-
 import { faTrash } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import {
@@ -17,54 +16,51 @@ import {
 } from "@tremor/react";
 
 import { deleteCategory } from "@/actions/categories";
-import ToastNotification from "@/components/toast-notification";
 
 export default function CategoriesTable({ title, tableData }: { title: string, tableData: any[]}) {
 
   const handleDelete = async (id: number) => {
     deleteCategory(id)
       .then( res => {
-        console.log(res);
-        toast.success("Producto eliminado")
+        toast.success(res.success)
       }).catch( error => {
         console.log(error);
-        toast.error("No se pudo eliminar el producto")
+        toast.error(error)
       } )
   }
 
   return (
-    <>
-      <Card className="border border-rose-100/40 rounded-xl">
-        <Title className="text-xl">{ title }</Title>
-        <Table className="mt-5">
-          <TableHead>
-            <TableRow>
-              <TableHeaderCell>Nombre</TableHeaderCell>
-              <TableHeaderCell>Descripcion</TableHeaderCell>
+    <Card className="h-full">
+      <Title className="text-xl">{ title }</Title>
+      <Table className="h-full border border-rose-100/40 rounded-xl mt-5">
+        <TableHead>
+          <TableRow>
+            <TableHeaderCell>Nombre</TableHeaderCell>
+            <TableHeaderCell>Descripcion</TableHeaderCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {tableData.map((prod: any) => (
+            <TableRow key={prod.id} className="hover:bg-rose-100/20">
+              <TableCell>
+                {prod.name}
+              </TableCell>
+              <TableCell>
+                <Text>{prod.description}</Text>
+              </TableCell>
+              <TableCell className="flex gap-3">
+                  <span
+                    className="flex items-center gap-1 cursor-pointer text-red-600"
+                    onClick={() => handleDelete(prod.id)}
+                  >
+                    <FontAwesomeIcon icon={faTrash} className="w-4 h-4" />
+                    Eliminar
+                  </span>
+              </TableCell>
             </TableRow>
-          </TableHead>
-          <TableBody>
-            {tableData.map((prod: any) => (
-              <TableRow key={prod.id} className="hover:bg-rose-100/10">
-                <TableCell>{prod.name}</TableCell>
-                <TableCell>
-                  <Text>{prod.description}</Text>
-                </TableCell>
-                <TableCell className="flex gap-3">
-                    <span
-                      className="flex items-center gap-1 cursor-pointer text-red-600"
-                      onClick={() => handleDelete(prod.id)}
-                    >
-                      <FontAwesomeIcon icon={faTrash} className="w-4 h-4" />
-                      Eliminar
-                    </span>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </Card>
-      <ToastNotification />
-    </>
+          ))}
+        </TableBody>
+      </Table>
+    </Card>
   )
 }
