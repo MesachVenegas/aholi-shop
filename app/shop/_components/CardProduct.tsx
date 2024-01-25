017b3816d62aeab4beb14afa02b0b37eada3ff5e
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-01-24 21:14:37 -0800

    create cart content list

--- app/shop/_components/CardProduct.tsx
+++ app/shop/_components/CardProduct.tsx
@@ -1,20 +1,27 @@
 'use client'
 
-import Image from 'next/image'
-import { Title5 } from '@/components/ui/Titles'
-import { valueFormatter } from '@/libs/utils'
-import { faCartPlus, faTruckFast } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { ProductProps } from '@/types/product'
+import Image from 'next/image';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartPlus, faTruckFast } from '@fortawesome/free-solid-svg-icons';
+
+import { valueFormatter } from '@/libs/utils';
+import { ProductProps } from '@/types/product';
+import { Title5 } from '@/components/ui/Titles';
+import { useAppDispatch } from '@/hooks/useRedux';
+import { addProductToCart } from '@/redux-store/features/cart/cartSlice';
+import Link from 'next/link';
 
 export default function CardProduct({ prod } : { prod: ProductProps }) {
+  const dispatch = useAppDispatch()
 
   return (
-    <figure className="flex flex-col p-2 w-96 h-[500px] border border-rose-700 rounded-lg cursor-pointer">
+    <figure className="flex flex-col p-2 w-full sm:max-w-[500px] md:max-w-[350px] lg:h-[500px] border border-rose-700 rounded-lg cursor-pointer">
       <div className="w-full h-[300px] relative">
         <Image
           src={`${prod?.images ? prod.images : "/products/default_img.png"}`}
           fill={true}
+          className='object-contain'
           alt='alt'
         />
       </div>
@@ -33,12 +40,20 @@ export default function CardProduct({ prod } : { prod: ProductProps }) {
           </span>
         </div>
         <div className='flex flex-col md:flex-row gap-2 mt-5'>
-          <button type='button' className="flex justify-center items-center bg-rose-700/80 hover:bg-rose-700 p-2 rounded-md text-white w-full md:max-w-[200px] gap-2">
+          <Link
+            href='/checkout'
+            type='button'
+            className="flex justify-center items-center bg-rose-700/80 hover:bg-rose-700 p-2 rounded-md text-white text-sm w-full md:max-w-[200px] gap-2"
+          >
             <FontAwesomeIcon icon={faTruckFast} className='w-5 h-5' />
             Comprar
-          </button>
-          <button type='button' className="flex justify-center items-center bg-rose-700/80 hover:bg-rose-700 p-2 rounded-md text-white w-full md:max-w-[200px] gap-2">
-            <FontAwesomeIcon icon={faCartPlus} className='w-5 h-5' />
+          </Link>
+          <button
+            type='button'
+            className="flex justify-center items-center bg-rose-700/80 hover:bg-rose-700 p-2 rounded-md text-white text-sm w-full md:max-w-[200px] gap-2"
+            onClick={ () => dispatch(addProductToCart(prod))}
+          >
+            <FontAwesomeIcon icon={faCartPlus} className='w-5 h-5'/>
             Agregar al carrito
           </button>
         </div>
